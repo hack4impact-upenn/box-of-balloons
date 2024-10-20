e871767ba3c51ac2eeedcb864bf779efc0291601
Caroline Chen <64424139+carolineychen8@users.noreply.github.com>
2024-10-20 16:45:25 -0400

    backend routes

---
+++ server/src/controllers/chapter.controller.ts
@@ -0,0 +1,48 @@
+/* eslint-disable import/prefer-default-export */
+import express from 'express';
+import ApiError from '../util/apiError.ts';
+import StatusCode from '../util/statusCode.ts';
+import {
+  toggleRequestByID,
+  getAllChaptersFromDB,
+} from '../services/chapter.service.ts';
+
+const getAllChapters = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  return (
+    getAllChaptersFromDB()
+      .then((userList) => {
+        res.status(StatusCode.OK).send(userList);
+      })
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .catch((e) => {
+        next(ApiError.internal('Unable to retrieve all users'));
+      })
+  );
+};
+
+const toggleRequest = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  const { id } = req.params;
+  if (!id) {
+    next(ApiError.missingFields(['id']));
+    return;
+  }
+
+  toggleRequestByID(id)
+    .then(() => {
+      res.sendStatus(StatusCode.OK);
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .catch((e) => {
+      next(ApiError.internal('Unable to toggle status.'));
+    });
+};
+
+export { toggleRequest, getAllChapters };
