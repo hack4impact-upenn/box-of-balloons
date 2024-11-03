29bb05da4e20b076db9750276c38e865d1f77877
annabay04 <annabay2004@gmail.com>
2024-11-03 16:44:16 -0500

    frontend admin dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,6 +22,7 @@ import EmailResetPasswordPage from './Authentication/EmailResetPasswordPage.tsx'
 import ResetPasswordPage from './Authentication/ResetPasswordPage.tsx';
 import AlertPopup from './components/AlertPopup.tsx';
 import InviteRegisterPage from './Authentication/InviteRegisterPage.tsx';
+import TempAdminDashboardPage from './AdminDashboard/TempAdminDashboardPage.tsx';
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
                   {/* Routes accessed only if user is not authenticated */}
                   <Route element={<UnauthenticatedRoutesWrapper />}>
                     <Route path="/login" element={<LoginPage />} />
+                    <Route path="/admin" element={<TempAdminDashboardPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route
                       path="/verify-account/:token"
