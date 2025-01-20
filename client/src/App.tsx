9bf7c520c2b7f5239b34710c76b7874c2b4d7821
kygchng <kygchng@gmail.com>
2025-01-20 15:57:01 -0500

    package.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
                     <Route path="/login" element={<LoginPage />} />
                     {/* <Route path="/admin" element={<TempAdminDashboardPage />} /> */}
                     {/* <Route path="/admin" element={<TempAdminDashboardPage />} /> */}
-                    <Route path="/register" element={<RegisterPage />} />
+                    {/* <Route path="/register" element={<RegisterPage />} /> */}
                     <Route
                       path="/verify-account/:token"
                       element={<VerifyAccountPage />}
